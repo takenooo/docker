4736398dc0b348d11b259ce6c964446f104e0256
takenooo <takenood+github@gmail.com>
2019-03-05 14:00:09 +0000

    Modify: create hw-daemon.ini if not exist.

---
+++ openvz-web-panel/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ ! -f /opt/ovz-web-panel/utils/hw-daemon/hw-daemon.ini ]; then
+    cp -p /opt/ovz-web-panel/utils/hw-daemon/hw-daemon.ini.sample /opt/ovz-web-panel/utils/hw-daemon/hw-daemon.ini
+fi
+
+/opt/ovz-web-panel/script/owp start
+
+while true; do
+    sleep 1
+done
